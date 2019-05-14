c2951864e2c0980f002dcb1ecc3c42150390dc6b
Devit951 <devit951@gmail.com>
2019-05-14 15:23:01 +0500

    Added FromRightToLeftFillingDirection

---
+++ fillingbutton/src/main/java/com/github/devit951/fillingbutton/direction/FromRightToLeftFillingDirection.kt
@@ -0,0 +1,13 @@
+package com.github.devit951.fillingbutton.direction
+
+import com.github.devit951.fillingbutton.FillingButton
+
+class FromRightToLeftFillingDirection: FillingDirection {
+    override fun drawDirection(fillingButton: FillingButton, animatedIntValue: Int){
+        fillingButton.fillingRect.left = fillingButton.viewWidth - animatedIntValue
+    }
+
+    override fun resetDirection(fillingButton: FillingButton) {
+        fillingButton.fillingRect.left = 0
+    }
+}
